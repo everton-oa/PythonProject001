e95140e91ff4bc236b008030ce7d0fd41c9c6ed8
Everton Araujo <everton.araujo@hp.com>
2021-06-04 06:38:27 -0300

    Add class with first test

--- tests/teste_ambiente.py
+++
@@ -1,8 +0,0 @@
-import os
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-print(os.getcwd())
-driver.get("file://" + os.getcwd() + "/resources/componentes.html")
-driver.find_element_by_id("elementosForm:nome").send_keys("testes")
-# driver.quit()

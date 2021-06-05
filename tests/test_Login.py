27218d9a176d8338c49cb67b8dab268c44f4f12d
Everton Araujo <everton.oa@gmail.com>
2021-06-05 10:32:15 -0300

    Change code to run testing environment - running ok

---
+++ tests/test_Login.py
@@ -0,0 +1,30 @@
+from pages.LoginPage import LoginPage
+from pages.HomePage import HomePage
+from selenium import webdriver
+import time
+import unittest
+
+
+class LoginTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(10)
+
+    def test_login_valid(self):
+        driver = self.driver
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_on_login_button()
+
+        homePage = HomePage(driver)
+        homePage.click_on_welcome()
+        homePage.click_on_logout()
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(2)
+        cls.driver.close()
+        cls.driver.quit()

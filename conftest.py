0dd19a93fa8ff691b7e2d0c76a2f94c5fe4815db
Everton Araujo <everton.oa@gmail.com>
2021-06-16 13:57:42 -0300

    Change teste_de_ambiente.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 from pytest import fixture
 from selenium import webdriver
-import os
 
 
 @fixture(scope="class")
@@ -9,4 +8,4 @@ def init_browser(request):
     request.cls.driver = web_driver
     web_driver.implicitly_wait(10)
     yield
-    web_driver.close()
+    web_driver.quit()

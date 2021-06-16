f4e07826acc44c89db93840f6dd63462f06b6511
Everton Araujo <everton.oa@gmail.com>
2021-06-16 14:00:03 -0300

    Add method maximize window to conftest.py

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 @fixture(scope="class")
 def init_browser(request):
     web_driver = webdriver.Chrome()
+    web_driver.maximize_window()
     request.cls.driver = web_driver
     web_driver.implicitly_wait(10)
     yield
--- teste_ambiente.py
+++ teste_ambiente.py
@@ -1,7 +1,5 @@
 import os
-import time
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 
 
 driver = webdriver.Chrome()

d0a2753b4352f8d93e7d9a3c41152920486e3b16
Everton Araujo <everton.oa@gmail.com>
2021-06-17 16:41:06 -0300

    Add driver manager implementation

--- teste_ambiente.py
+++ teste_ambiente.py
@@ -6,4 +6,9 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.maximize_window()
 driver.get("file://" + os.getcwd() + "/resources/componentes.html")
+import pdb
+
+pdb.set_trace()
+print(driver.current_url)
+driver.find_element_by_id("")
 driver.quit()

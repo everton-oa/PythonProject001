ba8702734ce2114b23bf531e737f6bddbdbfba41
Everton Araujo <everton.oa@gmail.com>
2021-06-20 10:46:19 -0300

    Add valid login test - needs assertion

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,16 @@
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
     def __init__(self, init_browser):
         self.driver = init_browser
+
+    def get_element(self, by_locator):
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(by_locator)
+        )
 
     def open_url(self, url):
         self.driver.get(url)
@@ -16,29 +22,29 @@ class BasePage:
         return self.driver.title
 
     def click(self, by_locator):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(by_locator)
-        ).click()
+        self.get_element(by_locator).click()
 
-    def send_key(self, by_locator, value):
+    def click_checkbox(self, by_locator):
+        self.click(by_locator)
+
+    def click_radio(self, by_locator):
+        self.click(by_locator)
+
+    def send_keys(self, by_locator, value):
         self.clear_input_field(by_locator)
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(by_locator)
-        ).send_keys(value)
+        self.get_element(by_locator).send_keys(value)
 
     def clear_input_field(self, by_locator):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(by_locator)
-        ).clear()
+        self.get_element(by_locator).clear()
 
     def get_element_text(self, by_locator):
-        element = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(by_locator)
-        )
+        element = self.get_element(by_locator)
         return element.text
 
     def is_enabled(self, by_locator):
-        element = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(by_locator)
-        )
+        element = self.get_element(by_locator)
         return bool(element)
+
+    def select(self, by_locator, value):
+        select = Select(self.get_element(by_locator))
+        return select.select_by_visible_text(value)
